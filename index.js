42609e5f63d615fb6bfd2ce9338f8411133ea17e
Poison010101 <lordhunter017@gmail.com>
2023-08-19 14:53:08 +0545

    Portfolio

---
+++ index.js
@@ -0,0 +1,32 @@
+// gsap.from(".container", {
+//     y : -100,
+//     delay: 0.10,
+//     // opacity: 0,
+// })
+// gsap.from("#Home .row",
+// {
+//     x : -200,
+//     duration: .8 , ease:"power3"
+//     });
+// gsap.from("#Home h6",
+// {
+//     x:-500,
+//     duration:.10,ease:'back',delay:.4,
+
+// })
+// gsap.from("#Home, h1",
+// {
+//     x : -100,
+//     // y : 500,
+//     duration : .10,
+//     ease :"elastic(1,.7)",
+//     stagger: -.1
+
+// })
+// // gsap.from("#aboutme h3",
+// // {
+// //     y : -100,
+// //     duration: .10,
+// //     opacity: 0,
+// // })
+
